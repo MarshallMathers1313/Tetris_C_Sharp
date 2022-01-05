32ff882b962fc2fa028bb2a8bc9cb7e13fec5bf9
MarshallMathers1313 <73746152+MarshallMathers1313@users.noreply.github.com>
2022-01-05 12:49:51 +0500

    Version 1.0

---
+++ Tetris/Tetris/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FieldPictureBox = new System.Windows.Forms.PictureBox();
+            this.TickTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.FieldPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FieldPictureBox
+            // 
+            this.FieldPictureBox.Location = new System.Drawing.Point(14, 16);
+            this.FieldPictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FieldPictureBox.Name = "FieldPictureBox";
+            this.FieldPictureBox.Size = new System.Drawing.Size(300, 427);
+            this.FieldPictureBox.TabIndex = 0;
+            this.FieldPictureBox.TabStop = false;
+            // 
+            // TickTimer
+            // 
+            this.TickTimer.Enabled = true;
+            this.TickTimer.Interval = 250;
+            this.TickTimer.Tick += new System.EventHandler(this.TickTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 488);
+            this.Controls.Add(this.FieldPictureBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Tetris";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.FieldPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox FieldPictureBox;
+        private System.Windows.Forms.Timer TickTimer;
+    }
+}
+
